b5ec32e07039876619474ed374a76b032fff9b4d
Saiko Ruslan <vamosbarca2407@gmail.com>
2024-10-23 14:29:34 +0300

    save

--- src/components/cardBook.vue
+++ src/components/cardBook.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="cardbook">
+    <div class="cardbook cardbook__container">
         <ul v-for="book in BookStore.books" :key="book.id">
             <li class="cardbook__title">{{ book.title }}</li>
             <li><img class="cardbook__image" :src="book.img" alt="Book cover" /></li>
             <li>{{ book.author }}</li>
-            <li>{{ book.rating }}</li>
+            <li ><i v-for="n in 5" :key="n" :class="n<=book.rating 
+            ? 'fa-solid fa-star ' : 'fa-regular fa-star '" @click="BookStore.edit_star(n,book)">
+                </i></li>
             <li>{{ book.is_read }}</li>
         </ul>
         <ul>
--- src/store/index.js
+++ src/store/index.js
@@ -3,17 +3,29 @@ import {ref} from 'vue';
 
 export const useBookStore = defineStore('gallery', ()=>{
     const books =ref([
-        {id:1, title: "Fire and sword", img: new URL('@/assets/img/fire_and_sword.jpg', import.meta.url).href,author: "Senkevich",rating:0,is_read:false},
+        {id:1, title: "Fire and sword", img: new URL('@/assets/img/fire_and_sword.jpg', import.meta.url).href,author: "Senkevich",rating:2,is_read:false},
         {id:2, title: "War and Peace",img: new URL('@/assets/img/war_and_peace.jfif', import.meta.url).href,author: "Tolstoy",rating:0,is_read:false},
         {id:3, title: "The truth about the Harry Quebert case",img: new URL('@/assets/img/true_about_diccer.jpg', import.meta.url).href,author: "Dicker",rating:0,is_read:false},
+        {id:4, title: "The truth about the Harry Quebert case",img: new URL('@/assets/img/true_about_diccer.jpg', import.meta.url).href,author: "Dicker",rating:0,is_read:false},
     ]);
-
-    function add_to_book(newBook) {
+    const loadBooks = () => {
+        const savedBooks = localStorage.getItem('book');
+        if(savedBooks){
+            books.value = JSON.parse(savedBooks);
+        }
+    }
+    const add_to_book = (newBook) =>{
         books.value.push(newBook);
-    }
+    };
 
-    function remove_book(itemId){
+    const remove_book = (itemId) =>{
         books.value = books.value.filter(item => item.id !== itemId)
-    }
-    return {books,add_to_book,remove_book}
+    };
+
+    const edit_star = (n,book) => {
+        book.rating = n;
+        localStorage.setItem('rating',JSON.stringify(book.rating));
+};
+loadBooks();
+  return {books,add_to_book,remove_book,edit_star,loadBooks};
 });
